07b73608f2b9cbf2b5d7526d50446e272d7c27f0
diego quiroz <ingquirozramirez@gmail.com>
2021-08-07 14:45:23 -0500

    .

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,25 +1,54 @@
 pipeline_template = "template_default"
 
-agent any
-
 libraries{
-    npm {
-        image_tag = "node:10.16.3-alpine"
-        dockerfile = "./Dockerfile"
-        docker_compose = "./docker-compose.yaml"
-        example = "./example.sh"
-    }
+    //npm {
+    //    image_tag = "node:10.16.3-alpine"
+    //}
+    // si usa infra. cloud serverless
+    /*serverless {
+        enforce_deploy = true
+    }*/
     sonarqube
-    ansible
+    /*email {
+        to = "example@admin.com"
+    }*/
 }
 
 application_environments{
     dev{
-        long_name = "Development"
-        ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
+        //si es en la nube aws
+        aws {
+            credentials_id = "*****"
+            stage = "DESA"
+        }
+        // si es on-premise
+        on_premise {
+            ip_address = "XYZ.XYZ.XYZ.XYZ"
+            port = "ZZZZ"
+        }
+    }
+    /*test{
+        //si es en la nube aws
+        aws {
+            credentials_id = "*****"
+            stage = "TEST"
+        }
+        // si es on-premise
+        on_premise {
+            ip_address = "XYZ.XYZ.XYZ.XYZ"
+            port = "ZZZZ"
+        }
     }
     prod{
-        long_name = "Production"
-        ip_addresses = [ "0.0.1.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
-    }
+        //si es en la nube aws
+        aws {
+            credentials_id = "*****"
+            stage = "PROD"
+        }
+        // si es on-premise
+        on_premise {
+            ip_address = "XYZ.XYZ.XYZ.XYZ"
+            port = "ZZZZ"
+        }
+    }*/
 }
